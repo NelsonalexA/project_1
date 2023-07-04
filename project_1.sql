a511148e4de595910a0425fbea40fa1c5c45f932
Nelson alex.A <129277706+NelsonalexA@users.noreply.github.com>
2023-07-04 10:45:57 +0530

    Update project_1.sql

--- project_1.sql
+++ project_1.sql
@@ -1,23 +1,28 @@
-create database me;
-create table me.student(
-SN_NO int primary key, 
-NAME varchar(20),
-ROLL_NO varchar(20),
-DEPARTMENT varchar(20),
-COLLEGE_NAME varchar(20),
-PLACE varchar(20)
-);
+/* to create database*/
+create database vnk;
 
-select * from me.student;
-drop table me.student;
-insert into me.student values
-(1,"Nelson",12,"MDE","MEC","Rasipuram"),
-(2,"Karthi",8,"MDE","MEC","Rasipuram"),
-(3,"Abi",1,"MDE","MEC","Rasipuram"),
-(4,"Ram",20,"MDE","MEC","Rasipuram"),
-(5,"Akil",2,"MDE","MEC","Rasipuram"),
-(6,"Mano",18,"MDE","MEC","Rasipuram"),
-(7,"Mugil",14,"MDE","MEC","Rasipuram"),
-(8,"Suriya",27,"MDE","MEC","Rasipuram"),
-(9,"NavIN",25,"MDE","MEC","Rasipuram"),
-(10,"Prem",27,"MDE","MEC","Rasipuram");
+/* to create a table*/
+create table vnk.sp(
+sl_No int,
+name varchar(20),
+Roll_No int,
+Department varchar(20),
+College_Name varchar(20),
+Place varchar(20));
+
+/to insert the values in table/ 
+insert into vnk.sp values
+(1,"naveenkumar.V",01,"B.E-Mechanical","MEC","Rasipuram"),
+(2,"kumar",02,"B.E-Mechanical","MEC","Rasipuram"),
+(3,"naveen",03,"B.E-Mechanical","MEC","Rasipuram"),
+(4,"lokesh",04,"B.E-Mechanical","MEC","Rasipuram"),
+(5,"Nelson",05,"B.E-Mechanical","MEC","Rasipuram"),
+(6,"abi",06,"B.E-Mechanical","MEC","Rasipuram"),
+(7,"siva",07,"B.E-Mechanical","MEC","Rasipuram"),
+(8,"kavin",08,"B.E-Mechanical","MEC","Rasipuram"),
+(9,"nithish",09,"B.E-Mechanical","MEC","Rasipuram"),
+(10,"arun",010,"B.E-Mechanical","MEC","Rasipuram");
+
+/to display a table/
+select * from vnk.sp;
+drop table vnk.sp;
